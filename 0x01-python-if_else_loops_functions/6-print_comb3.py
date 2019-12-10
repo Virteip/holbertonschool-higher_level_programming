136edd1ba1aea26859a15b91b587b3644465018c
virteip <sergiopietri@gmail.com>
2019-12-10 13:04:48 -0500

    [#6 (1): removed double 89]

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for num in range(0, 100):
     b = num % 10
 
     if a < b and a != b and num != 89:
-        print("{0:0=2d}, ".format(num), end='')
+       print("{0:0=2d}, ".format(num), end='')
     if num == 89:
-       print("{0}".format(num), end='')
+       print("{0}".format(num))
