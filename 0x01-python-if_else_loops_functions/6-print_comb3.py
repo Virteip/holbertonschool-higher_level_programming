f93f2f18f125105fa0d52b8292802d0dfa9c0745
virteip <sergiopietri@gmail.com>
2019-12-10 12:56:17 -0500

    [#6 (0)]

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num in range(0, 100):
+    a = num / 10
+    b = num % 10
+
+    if a < b and a != b:
+        print("{0:0=2d}, ".format(num), end='')
+    if num == 89:
+        print("{0}".format(num), end='')
