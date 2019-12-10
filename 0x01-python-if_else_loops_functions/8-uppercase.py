3c612f4051b9b97c9fc4153f29eeb42da569524e
virteip <sergiopietri@gmail.com>
2019-12-10 14:33:04 -0500

    [#8 (0)]

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+
+    for letter in str:
+
+        if ord(letter) > 96 and ord(letter) < 123:
+            letter = chr((ord(letter) - 32))
+
+        print("{}".format(letter), end='')
+
+    print()
