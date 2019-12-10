5979193fbb228b879c446770f3a0d2d33b9d4d5c
virteip <sergiopietri@gmail.com>
2019-12-10 13:18:36 -0500

    [#7 (1)]

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 def islower(c):
-    letters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
-    if c in letters:
+    if ord(c) < 123 and ord(c) > 96:
         return True
     else:
         return False
