e0dc9e3aa60d27a370fb2d64b3dacff81fd6e760
virteip <sergiopietri@gmail.com>
2019-12-10 13:05:45 -0500

    [#6 (3): fixed pep8]

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for num in range(0, 100):
     a = num / 10
     b = num % 10
 
-    if a < b and a != b and num != 89:
-       print("{0:0=2d}, ".format(num), end='')
-    if num == 89:
-       print("{0}".format(num))
+if a < b and a != b and num != 89:
+    print("{0:0=2d}, ".format(num), end='')
+if num == 89:
+    print("{0}".format(num))
