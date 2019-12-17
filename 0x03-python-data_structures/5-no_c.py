f1f0a864cb1e75c49f686703565af43393b45cb9
Virteip <sergiopietri@gmail.com>
2019-12-17 22:58:51 +0000

    [#5 (0)]

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    new = ""
+    chars = "Cc"
+    counter = 0
+
+    for iterate in my_string:
+        if iterate not in chars:
+            new = new + my_string[counter]
+        counter = counter + 1
+
+    return new
