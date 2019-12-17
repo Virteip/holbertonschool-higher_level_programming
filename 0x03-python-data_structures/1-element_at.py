e8c81e88fba98889a5b7f59ec0895c53b045c0ee
Virteip <sergiopietri@gmail.com>
2019-12-17 20:41:31 +0000

    [#1 removed second condition]

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,13 +2,7 @@
 
 
 def element_at(my_list, idx):
-    i = 0
-
-    for i in my_list:
-        i = i+1
-
-    if idx < 0 or idx > i:
+    if idx < 0 or idx > len(my_list):
         return None
 
-    if idx in my_list:
-        return my_list[idx]
+    return my_list[idx]
