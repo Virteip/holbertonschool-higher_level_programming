ebee107ef9b341ce1e2153177f27fe76b5f28b99
Virteip <sergiopietri@gmail.com>
2019-12-18 00:45:11 +0000

    #6 (2)

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    a = " "
-    for i in matrix:
-        for j in i:
-            print("{}".format(j), end='')
-            if j != i[-1]:
-                print("{}".format(a), end='')
-        print()
+
+    if matrix:
+        separate = " "
+        for i in matrix:
+            for j in i:
+                print("{}".format(j), end='')
+                if j != i[-1]:
+                    print("{}".format(separate), end='')
+            print()
