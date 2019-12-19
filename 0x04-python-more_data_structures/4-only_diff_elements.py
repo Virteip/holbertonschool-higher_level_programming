fea9545ed0d31fc3de848d6a17d866e037960816
Virteip <sergiopietri@gmail.com>
2019-12-19 23:32:43 +0000

    #4 different sintac

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,6 +2,5 @@
 
 
 def only_diff_elements(set_1, set_2):
-    l1 = [x for x in set_1 if x not in set_2]
-    l2 = [x for x in set_2 if x not in set_1]
-    return l1 + l2
+    return [x for x in set_1 if x not in set_2] + \
+        [x for x in set_2 if x not in set_1]
