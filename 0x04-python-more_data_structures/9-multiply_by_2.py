de5ffb365d5b962fca5722504df8f8e0a4b16a9a
Virteip <sergiopietri@gmail.com>
2019-12-20 02:07:58 +0000

    #9 (0)

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+        dictionary = a_dictionary
+        dictionary.update((key, value*2) for key, value in dictionary.items())
+        return dictionary
