cdc2862f7b6431d8cc8ba0ccd3a7ebfe373f935e
Virteip <sergiopietri@gmail.com>
2019-12-20 02:09:44 +0000

    #9 (1)

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 
 
 def multiply_by_2(a_dictionary):
-        dictionary = a_dictionary
+        dictionary = a_dictionary.copy()
         dictionary.update((key, value*2) for key, value in dictionary.items())
         return dictionary
