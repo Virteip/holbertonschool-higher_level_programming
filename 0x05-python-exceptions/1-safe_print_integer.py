c9e1cd2d24295a38ff922ca1bb9c8fa29bcf89cf
Virteip <sergiopietri@gmail.com>
2020-01-08 15:27:43 -0500

    [#1 (0)]

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        if type(value) == int:
+            print("{:d}".format(value))
+            return True
+    except SyntaxError:
+        print("Error")
