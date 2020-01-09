c10866f7c094cd1998e942b90660b29a0efbc9bb
Virteip <sergiopietri@gmail.com>
2020-01-09 16:32:47 -0500

    [#1 (0)]

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class to define a Square with private size variable
+"""
+
+
+class Square:
+    """
+    Init method to set size var
+    """
+    def __init__(self, size):
+        self.size = size
