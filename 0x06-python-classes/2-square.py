61ad41170a64b003528dfdedf5752dacbc78a158
Virteip <sergiopietri@gmail.com>
2020-01-09 17:37:46 -0500

    [#2 (0)]

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Class to define a Square with private size variable
+"""
+
+
+class Square:
+    """
+    Init method to set size variable and exceptions to be raised in case
+    conditions are not met.
+    """
+    def __init__(self, size=0):
+        if type(size) == int and size >= 0:
+            self.__size = int(size)
+        elif type(size) != int:
+            raise NameError("size must be an integer")
+        elif size < 0:
+            raise NameError("size must be >= 0")
