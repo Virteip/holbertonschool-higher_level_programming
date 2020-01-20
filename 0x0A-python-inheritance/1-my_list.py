6304526dd52d8e580c20a2a3907046a47e6af62c
Virteip <sergiopietri@gmail.com>
2020-01-20 18:02:39 -0500

    [#1 0]

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Create Class MyList
+"""
+
+
+class MyList(list):
+    """
+    Define method to order list by copying it
+    """
+    def print_sorted(self):
+        copy = self.copy()
+        copy.sort()
+        print(copy)
