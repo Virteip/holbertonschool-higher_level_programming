b5eebdf1f82bd81daf0af5a7eadeeccbd8bc6953
Virteip <sergiopietri@gmail.com>
2020-01-20 20:35:08 -0500

    [#10 0]

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Raise an exception for area not defined
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """
+    Validate Value type and #
+    """
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        elif value <= 0:
+            raise ValueError(name + " must be greater than 0")
+"""
+Class Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Constructor method initializer
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    """
+    Print string with str
+    """
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+    """
+    Calculate and return rectangle area
+    """
+    def area(self):
+        return self.__width * self.__height
+
+"""
+Calculate and return rectangle area
+"""
+
+
+class Square(Rectangle):
+    """
+    Constructor method initializer
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+    """
+    Calculate and return square area
+    """
+    def area(self):
+        return self.__size * self.__size
