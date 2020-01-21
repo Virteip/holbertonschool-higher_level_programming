12d36e69db9505b2e90d0acadfa18518060fbbf9
Virteip <sergiopietri@gmail.com>
2020-01-21 08:43:16 -0500

    [#101]

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+/**
+ * print_python_list_info - python list
+ *
+ * @p: PyObject
+ *
+ *
+ * Return: list
+ */
+void print_python_list_info(PyObject *p)
+{
+	unsigned long int i;
+
+	printf("[*] Size of the Python List = %lu\n",
+	       Py_SIZE(p));
+	printf("[*] Allocated = %lu\n",
+	       ((PyListObject *)p)->allocated);
+	for (i = 0; i < PyLis
+		     t_Size(p);)
+	{
+		printf("Element%d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->
+		       tp_name);
+		i++;
+	}
+}
