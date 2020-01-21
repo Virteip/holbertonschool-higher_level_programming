3571f69182a7616c836854532bab354df96bfa20
Virteip <sergiopietri@gmail.com>
2020-01-20 20:04:43 -0500

    [#8 0]

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Raise an exception for area not defined
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """
+    Validate Value type and #
+    """
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        elif value <= 0:
+            raise ValueError(name + " must be greater than 0")
+"""
+Class Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Constructor method initializer
+    """
+    def __init__(self, width, height):
+        if self.integer_validator("width", width) or\
+           self.integer_validator("height", height):
+            self.__width = width
+            self.__height = height
