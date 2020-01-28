eba1837b168154b3fbef66d55c722da40793d77f
Virteip <sergiopietri@gmail.com>
2020-01-28 18:25:07 +0000

    [add pep8]

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""
+Unit Tests
+"""
 import unittest
-from models.base import Base
 
 
-class TestStringMethods(unittest.TestCase):
-    def test_id_true(self):
-        self.assertEqual(Base(24).id, 24)
-        self.assertEqual(Base(2).id, 2)
-        self.assertEqual(Base(None).id, 1)
-        self.assertEqual(Base('8').id, '8')
-
-    def test_id_errors(self):
-        with self.assertRaises(TypeError):
-            Base(1, 1)
-        with self.assertRaises(AttributeError):
-            self.__nb_objects
-        self.assertRaises(TypeError, Base, exact=0)
+class TestBase(unittest.TesCase):
+    """
+    Test cases for Base module
+    """
+    def test_pep8(self):
+        """
+        Check PEP8
+        """
+        betty = pep8.StyleGuide(quiet=True)
+        check = betty.check_files(['models/base.py', 'models/rectangle.py',
+                                   'models/square.py'])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warnings).")
