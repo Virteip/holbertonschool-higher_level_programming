17bcf8b6fcbd9a6ad851e6c1fd441a28e10bdb04
Virteip <sergiopietri@gmail.com>
2020-03-02 22:44:34 +0000

    [Added T#15 add desc]

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- List score >= 10 from second_table
-SELECT score,COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score,COUNT(score) as number FROM second_table GROUP BY score DESC;
