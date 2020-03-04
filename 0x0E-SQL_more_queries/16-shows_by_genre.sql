ce9bcd29d8ae772d237659aff5e046b837f0b8b4
Virteip <sergiopietri@gmail.com>
2020-03-04 03:24:01 +0000

    [T#16]

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title, tv_genres.name ASC;
