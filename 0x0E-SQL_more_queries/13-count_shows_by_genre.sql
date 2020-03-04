bbe4e6e36cc1448404cc728e0213e4051a63e81e
Virteip <sergiopietri@gmail.com>
2020-03-04 14:24:02 +0000

    [T#13]

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre
---comment
 SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows FROM tv_show_genres
 INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
