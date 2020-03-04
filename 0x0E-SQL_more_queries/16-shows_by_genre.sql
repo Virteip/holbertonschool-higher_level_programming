ba127245f3946e3d565190f357a4dda712192f85
Virteip <sergiopietri@gmail.com>
2020-03-04 03:25:56 +0000

    [T#16]

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Select Comedy series
 SELECT tv_shows.title AS title, tv_genres.name AS name
-FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name ASC;
