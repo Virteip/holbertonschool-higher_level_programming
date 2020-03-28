a0efc842a43602e315c022bff546cfe32a6150dd
Virteip <sergiopietri@gmail.com>
2020-03-28 12:33:57 -0500

    [comment, pep8]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""
+pass name to look for in argv
+"""
 import MySQLdb
 from sys import argv
 
 
-if len(argv) - 1 == 4:
+if __name__ == "__main__":
 
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
 
-    cur.execute("SELECT id, name FROM states WHERE name LIKE '{:s}' ORDER BY id ASC;".format(argv[4]))
+    cur.execute("SELECT id, name FROM states WHERE name LIKE '{:s}'\
+    ORDER BY id ASC;".format(argv[4]))
 
     for result in cur:
         print(result)
-else:
-    pass
