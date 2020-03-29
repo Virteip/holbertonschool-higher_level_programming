d3b791d6314ad613c3028457347339a15b4ea947
Virteip <sergiopietri@gmail.com>
2020-03-29 14:08:21 -0500

    [T#15 delete delete-orphan]

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,4 +15,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan, delete")
+                          cascade="all, delete")
