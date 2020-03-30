8ca7c4e7b7b38df9412c87fb82ccdf10ed082a7b
Virteip <sergiopietri@gmail.com>
2020-03-30 10:13:34 -0500

    [pep8 according to checker even though mine doesnt say anything]

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host='localhost', user=argv[1],
                              passwd=argv[2], db=argv[3], port=3306)
         cur = db.cursor()
-        cur.execute("SELECT id, name FROM states WHERE name RLIKE '^N'\
+        cur.execute("SELECT id, name FROM states WHERE name LIKE BINARY 'N%'\
         ORDER BY id ASC;")
         query = cur.fetchall()
         for result in query:
