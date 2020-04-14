91063e8305a87ff8abab867346d427771c90fe72
Virteip <sergiopietri@gmail.com>
2020-04-14 16:27:20 -0500

    [Initialize folder]

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    resp = response.read()
+
+print('Body response:\n\t- type:{}\n\t- content: {}\n\t- utf8 content:{}'.
+      format(type(resp), resp, resp.decode("utf-8")))
