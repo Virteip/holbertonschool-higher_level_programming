c45db04d2e6c1ce98e698402702e40b9291b20cd
Virteip <sergiopietri@gmail.com>
2020-04-14 18:15:52 -0500

    [Add] T#2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+
+import urllib.request
+import sys
+
+email = {'email': sys.argv[2]}
+url = sys.argv[1]
+
+data = urllib.parse.urlencode(email)
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    resp = response.read().decode('utf-8')
+print("{}".format(resp))
