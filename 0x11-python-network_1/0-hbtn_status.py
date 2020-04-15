1138a4f98147ab1504e745cc4414f9a53eacb2b7
Virteip <sergiopietri@gmail.com>
2020-04-15 12:38:54 -0500

    [Update] idk...

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status
+"""Python script that fetches
 """
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         resp = response.read()
 
-    print('Body response:'.format())
-    print('    - type: {}'.format(type(resp)))
-    print('    - content: {}'.format(resp))
-    print('    - utf8 content: {}'.format(resp.decode("utf-8")))
+    print('Body response:\n\
+    - type: {}\n\
+    - content: {}\n\
+    - utf8 content: {}'.format(type(resp), resp, resp.decode("utf-8")))
