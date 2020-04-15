664e8b7927d475de3599213caadb8ae9f7678d9d
Virteip <sergiopietri@gmail.com>
2020-04-15 12:46:36 -0500

    [Update] remove extra space

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     resp = requests.get(url)
-    num_code =  resp.status_code
+    num_code = resp.status_code
     if num_code >= 400:
         print("Error code: {}".format(num_code))
     else:
