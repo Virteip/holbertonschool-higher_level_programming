fcbd294830c22ab8e297f11d81ed9c2a10e68305
Virteip <sergiopietri@gmail.com>
2020-04-15 12:44:21 -0500

    [Update] checker

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         resp = response.read()
 
-    print('Body response:\n\
+        print('Body response:\n\
     - type: {}\n\
     - content: {}\n\
     - utf8 content: {}'.format(type(resp), resp, resp.decode("utf-8")))
