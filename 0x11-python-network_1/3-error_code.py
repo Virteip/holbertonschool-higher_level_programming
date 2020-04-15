5492d33fc73b4ab9bdb872c369d07c406d058817
Virteip <sergiopietri@gmail.com>
2020-04-15 11:00:05 -0500

    [Add] T#3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        resp = response.read().decode('utf-8')
+    print("{}".format(resp))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
