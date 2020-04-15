94c54658ddb88e57c5ea171279a07ace418bb10e
Virteip <sergiopietri@gmail.com>
2020-04-15 12:18:26 -0500

    [Add] T#7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that displays error code for request
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    resp = requests.get(url)
+    num_code =  resp.status_code
+    if num_code >= 400:
+        print("Error code: {}".format(num_code))
+    else:
+        print(resp.text)
