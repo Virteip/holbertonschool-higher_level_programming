69d4dd0c8b07258d33e00de524e3852a00499450
Virteip <sergiopietri@gmail.com>
2020-04-16 11:57:42 -0500

    [Update] add authentication

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Python script that connects to github through the API and retrieves username
 """
+from requests.auth import HTTPBasicAuth
 import requests
 import sys
 
@@ -8,12 +9,11 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 3:
         user = sys.argv[1]
-        token = sys.argv[2]
+        pasw = sys.argv[2]
 
-    url = "https://api.github.com" + "/users/" + user
-
-    resp = requests.get(url)
+    resp = requests.get('https://api.github.com/user', auth=(user, pasw))
     content = resp.json()
+
     if content:
         print("{}".format(content.get('id')))
     else:
