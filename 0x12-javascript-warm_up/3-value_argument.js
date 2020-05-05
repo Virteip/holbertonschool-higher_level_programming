6a7f4e3ff120d48cd166addeb1da510a028b2500
Virteip <sergiopietri@gmail.com>
2020-05-05 11:17:33 -0500

    [Update] Remove length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   const myVar = process.argv.slice(2);
-
   console.log(myVar.join('\n'));
+} else {
+  console.log('No argument');
 }
