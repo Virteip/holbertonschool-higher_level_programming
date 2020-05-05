020a46b901f70ffcc2feeb204804bd7e715c6809
Virteip <sergiopietri@gmail.com>
2020-05-05 12:43:40 -0500

    [Fix] regex

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const intRegex = /^[0-9].[0-9]$/;
+const intRegex = /^[0-9]*?.[0-9]*$/;
 
 if (intRegex.test(process.argv[2])) {
   console.log('My number: ' + parseInt(process.argv[2]));
