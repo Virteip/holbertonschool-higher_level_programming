2df4f6cfc735964ed514351db458bfb7b3f4a2a7
Virteip <sergiopietri@gmail.com>
2020-05-05 12:31:04 -0500

    [Fix] Proper print

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const intRegex = /^\d+$/;
 
-if (intRegex.test(process.argv[2])) {
+if (intRegex.test(parseInt(process.argv[2]))) {
   console.log('My number: ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
