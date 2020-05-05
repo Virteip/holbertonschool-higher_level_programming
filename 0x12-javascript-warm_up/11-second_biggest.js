b62373a1cd75299e79b44cccddeef2f5294300c2
Virteip <sergiopietri@gmail.com>
2020-05-05 17:29:25 -0500

    [Update] ...

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,21 @@
 #!/usr/bin/node
 const intArray = process.argv.splice(2);
 const arrLength = intArray.length;
-let result;
+let result, i, j;
 
 function secondLargest (intArray) {
-  result = intArray.reverse().filter(num => num >= 0);
-  return result[1];
+  result = intArray.slice(0);
+  i = Math.max.apply(Math, result);
+  i = i - 1;
+
+  while (i > 0) {
+    for (j = 0; j < arrLength; j++) {
+      if (parseInt(i) === parseInt(result[j])) {
+        return result[j];
+      }
+    }
+    i--;
+  }
 }
 
 if (arrLength > 1) {
