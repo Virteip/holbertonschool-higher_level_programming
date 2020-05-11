f0cb75f33921492a28fcb56fd69eb6d6f406ffd7
Virteip <sergiopietri@gmail.com>
2020-05-11 13:59:31 -0500

    [Add]

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const SquareV2 = require('./5-square');
+
+class Square extends SquareV2 {
+  charPrint (c) {
+    const character = c ? 'C' : 'X';
+
+    for (let i = 1; i <= this.height; i++) {
+      console.log(character.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
