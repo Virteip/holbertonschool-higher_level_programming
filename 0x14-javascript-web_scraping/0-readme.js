962ebf46ba8ece521fa620ed6b8fa0949cda8eea
Virteip <sergiopietri@gmail.com>
2020-05-12 12:38:20 -0500

    [Fix]

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-fs.readFile(process.argv.slice(2)[0], (err, data) => {
+fs.readFile(process.argv.slice(2)[0], function (err, data) {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
