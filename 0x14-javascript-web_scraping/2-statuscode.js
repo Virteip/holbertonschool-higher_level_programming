f3b82e36b0a3a72bf7ac55651c1507d57e26a0b7
Virteip <sergiopietri@gmail.com>
2020-05-12 12:45:24 -0500

    [Fix] print

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ request(process.argv.slice(2)[0], function (error, response) {
   if (error != null) {
     console.error('error:', error);
   }
-  console.log('code: ', response.statusCode);
+  console.log(`code: ${response.statusCode}`);
 });
