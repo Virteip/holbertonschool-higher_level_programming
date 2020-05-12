5d7c73f38e6911091a4421c1955fd8a99e9abbff
Virteip <sergiopietri@gmail.com>
2020-05-12 12:54:03 -0500

    [Fix] undefined error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,8 +3,9 @@
 const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv.slice(2)[0], function (error, body) {
-  if (error != null) {
+  if (error === null) {
     console.error('error:', error);
+  }else{
+    console.log(JSON.parse(body.body).title);
   }
-  console.log(JSON.parse(body.body).title);
 });
