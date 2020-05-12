fcdfa608f30aaf26bb216d61a15061ab98eeb77c
Virteip <sergiopietri@gmail.com>
2020-05-12 11:43:41 -0500

    [Add]

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv.slice(2)[0], function (error, body) {
+  if (error != null) {
+    console.error('error:', error);
+  }
+  console.log(JSON.parse(body.body).title);
+});
