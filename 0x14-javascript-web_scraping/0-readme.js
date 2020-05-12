175657f80353d8dbf8114f438595b720001df12e
Virteip <sergiopietri@gmail.com>
2020-05-12 12:39:41 -0500

    [Fix] semi

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv.slice(2)[0],'utf-8', function (err, data) {
+fs.readFile(process.argv.slice(2)[0], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
