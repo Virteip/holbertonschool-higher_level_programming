4a1b76085596960899dde36c779265a234528ef3
Virteip <sergiopietri@gmail.com>
2020-05-12 23:46:34 -0500

    [Fix] ?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,18 @@
 const request = require('request');
 const nObj = {};
 
-request(process.argv.slice(2)[0], function (error, body, response) {
-  if (error) {
+request(process.argv.slice(2)[0], function (error, response, body) {
+  if (!error && body.length > 2) {
+    const contentJ = JSON.parse(body);
+
+    for (const key of contentJ) {
+      const id = key.userId;
+      if (nObj[id] === undefined) { nObj[id] = 0; }
+      if (key.completed === true) { nObj[id] += 1; }
+    }
+
     console.log(nObj);
   } else {
-    const contentJ = JSON.parse(response);
-
-    try {
-      for (const key of contentJ) {
-        const id = key.userId;
-        if (nObj[id] === undefined) { nObj[id] = 0; }
-        if (key.completed === true) { nObj[id] += 1; }
-      }
-    } catch (error) {
-      return;
-    }
     console.log(nObj);
   }
 });
